53fa12253a0e480b7668614b23d5fa0f71be8e93
RB <82284311+Roman123-game@users.noreply.github.com>
2022-04-01 18:37:33 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import axios from "axios";
 import "./App.css";
-import { useState, useMemo } from "react";
+import { useState, useEffect } from "react";
 import React from "react";
 
 function App() {
   const [posts, setPosts] = useState([]);
-  useMemo(() => {
-    return [...posts];
-  }, [posts]);
 
   async function fetchPost() {
     setPosts([]);
-    console.clear();
     const responce = await axios.get(
-      'https://jsonplaceholder.typicode.com/todos/1'
-    );
-    setPosts(responce.data);
-    console.log(responce)
-  }
+      'https://jsonplaceholder.typicode.com/todos'
+    )
+       console.log(responce);
+       setPosts(responce.data);
+
+     }
   function removePost(event) {
     const afterFilter = posts.filter( value => value.id != event.target.value );
     setPosts(afterFilter);
@@ -27,20 +24,22 @@ function App() {
   return (
     <div className="App">
       <button className="pressMe" onClick={fetchPost}>Press me</button>
-      {posts.map((post) => (
-        <div className="div"
-          key={post.id}>
-         <b className="bold"> {post.id}</b>
-          {post.title}
-           {post.description}
-          <button
-          className="buttonX"
-            value={post.id}
-            onClick={(event) => {removePost(event)}}>x</button>
-</div>
-      ))}
+      {[...posts].map((post) => (
+          <div className="div"
+            key={post.id}>
+           <b className="bold"> {post.id}</b>
+            {post.title}
+            <button
+            className="buttonX"
+              value={post.id}
+              onClick={(event) => {removePost(event)}}>x</button>
+        </div>))}
+
+
     </div>
   );
 }
 
 export default App;
+
+
