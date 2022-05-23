7f681f8eb8964389b57e712a895af8c071e1d667
RB <82284311+Roman123-game@users.noreply.github.com>
2022-05-23 10:11:38 +0300

    update

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import React from "react";
 
 function App() {
   const [posts, setPosts] = useState([]);
-  const [curentPost, setCurentPost] = useState("loading... please press again");
-  const [translatedPost,setTranslatedPost] = useState();
+  const [curentPost, setCurentPost] = useState("to translate click on post");
+  const [translatedPost, setTranslatedPost] = useState();
   const [page, setPage] = useState(1);
-  const [limit,setLimit] = useState(10);
+  const [limit, setLimit] = useState(10);
   const [forwardDisabled, setForwardDisabled] = useState(false);
   const [backwardDisabled, setBackwardDisabled] = useState(false);
 
   useEffect(() => {
-    fetchPost();
+    return fetchPost();
   }, [page]);
 
-  async function fetchPost(_limit = limit, _page = page) {
+  useEffect(() => {
+    return translate();
+  }, [curentPost]);
+
+  async function fetchPost(_limit = 10, _page = page) {
     const responce = await axios.get(
       "https://jsonplaceholder.typicode.com/todos",
       {
@@ -28,15 +32,36 @@ function App() {
     );
     setPosts(responce.data);
   }
+  async function translate() {
+    const axios = require("axios");
+    const options = {
+      method: "GET",
+      url: "https://just-translated.p.rapidapi.com/",
+      params: { lang: "en", text: String(curentPost) },
+      headers: {
+        "X-RapidAPI-Host": "just-translated.p.rapidapi.com",
+        "X-RapidAPI-Key": "1961caa73emshce7bd432426028bp14f970jsne28e0730d487",
+      },
+    };
+
+    await axios
+      .request(options)
+      .then(function (response) {
+        setTranslatedPost(response.data.text);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
+
   function removePost(event) {
     console.log(posts, event.target.value);
     const afterFilter = posts.filter(
       (value) => value.id !== parseInt(event.target.value)
     );
-    // value.id !== event.target.value
-
     setPosts(afterFilter);
   }
+
   function setForwardPage() {
     if (page >= 20) {
       setForwardDisabled(true);
@@ -46,6 +71,7 @@ function App() {
       setPage(page + 1);
     }
   }
+
   function setBackwardPage() {
     if (page <= 1) {
       setBackwardDisabled(true);
@@ -55,44 +81,20 @@ function App() {
       setPage(page - 1);
     }
   }
-  async function showCurrenPost(event) {
-    setCurentPost(event.target.innerText);
-    translate();
-  }
-
-  
-    async function translate() {
-      const axios = require("axios");
-      const options = {
-        method: 'GET',
-        url: 'https://just-translated.p.rapidapi.com/',
-        params: {lang: 'en', text: String(curentPost)},
-        headers: {
-          'X-RapidAPI-Host': 'just-translated.p.rapidapi.com',
-          'X-RapidAPI-Key': '1961caa73emshce7bd432426028bp14f970jsne28e0730d487'
-        }
-      };
-      
-     await axios.request(options).then(function (response) {
-       const afterJson =  JSON.stringify(response.data.text)
-        setTranslatedPost(response.data.text);
-      }).catch(function (error) {
-        console.error(error);
-      });
-    }
-  
 
   return (
     <div className="App">
-    <h3 className="lorem"> Lorem Ipsum Posts</h3>
-    <h1 className="map">&#x1F5FA;</h1>
-     <h6> *press on Post for translation</h6>
-      
+      <h3 className="lorem"> Lorem Ipsum Posts</h3>
+      <h1 className="map">&#x1F5FA;</h1>
+      <h6> *press on Post for translation</h6>
+
       {[...posts].map((post) => (
         <div className="id" key={post.id}>
           <div className="bold"> {post.id}</div>
-          <div className="title" onClick={(event) => showCurrenPost(event)}>
-            {" "}
+          <div
+            className="title"
+            onClick={(event) => setCurentPost(event.target.innerText)}
+          >
             {post.title}
           </div>
           <button
